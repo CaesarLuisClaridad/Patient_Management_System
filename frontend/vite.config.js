9aac220fd32cca4d3b15177e9f105e1ade38bcd8
Caesar Luis II M. Claridad <sesar091913@gmail.com>
2025-04-08 09:56:41 +0800

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://jhanice-birthday-1.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '') // Optionally rewrite the path
       }
