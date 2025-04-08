26c997b7edfb9d317325f58541a8311f78897095
Caesar Luis II M. Claridad <sesar091913@gmail.com>
2025-04-08 12:19:29 +0800

    tst

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -6,7 +6,8 @@ export default (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === "PRODUCTION", // Only secure in production
+    sameSite: "None", // Crucial for cross-origin requests
   };
   
   res.status(statusCode).cookie("token", token, options).json({
